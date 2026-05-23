001b7d28968e16a1d13ab5c0217dcd32d934fcfa
PhucNguyen-eng <nphuc4748@gmail.com>
2026-05-23 11:00:11 +0700

    first commit

---
+++ src/main/java/com/example/swp319seal/Swp319SealApplication.java
@@ -0,0 +1,13 @@
+package com.example.swp319seal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp319SealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swp319SealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swp319seal/Swp319SealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swp319seal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swp319SealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
